60431e2321a4c11a495308cfcbb7a21be2d69379
Antoni Roman <antonipaulo.roman@gmail.com>
2026-06-20 08:19:57 -0500

    add app middleware and users register/login routes

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
 import express from "express";
+import getUserFromToken from "#middleware/getUserFromToken";
+import usersRouter from "#routes/users";
+
 const app = express();
+
+app.use(express.json());
+app.use(getUserFromToken);
+
+app.use("/users", usersRouter);
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).send(err.message);
+});
+
 export default app;
